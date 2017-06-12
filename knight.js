0aba0e959d4c4bdb0638e167f597c61f1412d9b9
Alyssa Herzog <Alyssa.Herzog@Turner.com>
2017-06-12 09:29:48 -0400

    added wait-time and made gcp and nodeSSHShell default

--- knight.js
+++ knight.js
@@ -11,14 +11,14 @@ const program   = require('commander'),
 
 program.version(require('./package.json').version)
     .option("-c --config </path/to/config_file.yaml>", "Use config file instead of default ~/.knight/config.yml", (val) => path.normalize(val.replace("~",process.env.HOME)), process.env.HOME + "/.knight/config.yml")
-    .option("-t --type <machine_type>","Specify type of AWS image to spin up")
-    .option("-i --image <image_name>", "Specify AMI image to use.")
+    .option("-t --type <machine_type>","Specify type of AWS/GCP VM to spin up")
+    .option("-i --image <image_name>", "Specify AMI/GCP image to use.")
     .option("-I --identity <identity>", "Specify the path to the identity file to use for SCP and SSH.")
     .option("-t --tag <tag>", "Tag name to keep track of the instances")
     .option("-k --key <key>", "Public Key used for authorization")
     .option("-s --secret <secret>", "AWS Key Secret used for authorization")
-    .option("-r --region <region>", "AWS region to use")
-    .option("-g --gcp", "GCP Mode")
+    .option("-r --region <region>", "Cloud region to use")
+    .option("-g --gcp", "GCP Mode, currently on by default till AWS integration is complete", true)
     .option("-p --project-id <project_id>", "Project Id for GCP")
     .option("-K --keyfile <keyfile_path>", "Path to the authfile.json for GCP")
     .option("-P --prefix <prefix>","Prefix to use when naming VMs. Defaults to: siege-machine")
@@ -27,12 +27,13 @@ program.version(require('./package.json').version)
     .option("-f --file <path>", "Path to siege file to scp to the siege machines.")
     .option("-o --output <path>", "Path to an output file for the pdsh/nodeShell to send logs to.")
     .option("-u --username <username>","Username to use to SSH into. Defaults to 'knight'")
-    .option("-n --node-ssh", "If we should use a nodeJS specific SSH Client instead of PDSH (useful if pdsh isn't installed) defaults to false, will be used if pdsh isn't detected")
-    .option("--create-config", "Have knight create the config.yml file, specify with -c otherwise uses default location", (val) => true, false)
+    //.option("-n --node-ssh", "If we should use a nodeJS specific SSH Client instead of PDSH (useful if pdsh isn't installed) defaults to false, will be used if pdsh isn't detected")
+    .option("-w --wait-time <milliseconds>", "Time in milliseconds that knight will wait for the new VMs to boot up.")
+    .option("--create-config", "Have knight create the config.yml file, specify with -c otherwise uses default location, exits on completion", (val) => true, false)
     .option("-v --verbose", "Verbose flag, can be used multiple times.", (v,total) => total + 1, 0)
     .parse(process.argv);
 
-let opts = ["type","image","tag","key","secret","region","min","max","file", "identity", "projectId", "keyfile","prefix", "gcp", "username"].filter(k => program[k] !== undefined ).reduce((o,v) => { o[v] = program[v]; return o; }, {});
+let opts = ["type","image","tag","key","secret","region","min","max","file", "identity", "projectId", "keyfile","prefix", "gcp", "username", "waitTime"].filter(k => program[k] !== undefined ).reduce((o,v) => { o[v] = program[v]; return o; }, {});
 let defaults = {
   type: "t2.micro",
   image: "ami-c58c1dd3",
@@ -89,8 +90,8 @@ process.on('uncaughtException', err => {
   console.error(err);
   cloud.deleteVMs(opts,machines,() => {
     process.exit(1);
-  })
-})
+  });
+});
 
 async.waterfall([
   async.apply(cloud.generateVMs, opts),
